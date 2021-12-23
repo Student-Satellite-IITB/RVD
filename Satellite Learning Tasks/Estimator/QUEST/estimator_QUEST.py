982212a13a7a35dd4f81310a8259d42a093e5955
Adhyyan <76652860+adhyyanm@users.noreply.github.com>
2021-12-23 23:33:35 +0530

    Update estimator_QUEST.py
    split the code into specific functions

--- Satellite Learning Tasks/Estimator/QUEST/estimator_QUEST.py
+++ Satellite Learning Tasks/Estimator/QUEST/estimator_QUEST.py
@@ -13,28 +13,40 @@ I = np.identity(3)
 
 #taking all the weights to be 1
 
-lambda_0 = vector_B_st.shape[0]
-
-B = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0]])
-for i in range(vector_B_st.shape[0]):
-    B = B + np.outer(vector_B_st[i],vector_I_m[i])
-
-#print(B)
-
-S = B + np.transpose(B) 
-kai = np.trace(np.linalg.det(S)*np.linalg.inv(S))
-z = np.array([B[1][2]-B[2][1] , B[2][0] - B[0][2] ,B[0][1] - B[1][0]])
 
 #print(z)
 
-def func(lambda0):  #can't use lambda as that is apparently a function
+def B_func():
+    B = np.array([[0, 0, 0],[0, 0, 0],[0, 0, 0]])
+    for i in range(vector_B_st.shape[0]):
+        B = B + np.outer(vector_B_st[i],vector_I_m[i])
+    return B
+
+def S_func():
+    B = B_func()
+    S = B + np.transpose(B)
+    return S 
+
+def z_func():
+    B = B_func()
+    z = np.array([B[1][2]-B[2][1] , B[2][0] - B[0][2] ,B[0][1] - B[1][0]])
+    return z
+
+
+def func(lambda0, kai):  #can't use lambda as that is apparently a function
+    B = B_func()
+    S = S_func()
+    z = z_func()
     return ((lambda0**2 - np.trace(B)**2 + kai)*(lambda0**2 - np.trace(B)**2 - np.inner(z,z)) - (lambda0 - np.trace(B))*(np.matmul(np.matmul(z,S),np.transpose(z)) + np.linalg.det(S)) - np.matmul(np.matmul(z,np.matmul(S,S)),np.transpose(z)))
 
-def dfunc(lambda1):
+def dfunc(lambda1, kai):
+    B = B_func()
+    S = S_func()
+    z = z_func()
     return (2*lambda1*(lambda1**2 - np.trace(B)**2 - np.inner(z,z) + lambda1**2 - np.trace(B)**2 + kai) - (np.matmul(np.matmul(z,S),np.transpose(z)) + np.linalg.det(S)))
 
-def newtonRaphson( x ):
-    h = func(x) / dfunc(x)
+def newtonRaphson(x, kai):
+    h = func(x, kai) / dfunc(x, kai)
     while abs(h) >= 0.0001:  #our limits
         h = func(x)/dfunc(x)
         # x(i+1) = x(i) - f(x) / f'(x)
@@ -43,21 +55,27 @@ def newtonRaphson( x ):
  
 # Driver program to test above
  # Initial values assumed
-lambda_max = newtonRaphson(lambda_0)
 
-print(lambda_max)
+def main():
+    lambda_0 = vector_B_st.shape[0]
 
-rho = lambda_max + np.trace(B)
-temp = np.matmul(np.linalg.det(rho*I - S)*np.linalg.inv(rho*I - S), np.transpose(z))
-#print(temp)
+    B = B_func()
+    S = S_func()
 
-quaternion_estimate = np.array([[temp[0]],[temp[1]],[temp[2]],[np.linalg.det(rho*I - S)]])
-#print(quaternion_estimate)
-quaternion_estimate = (1/np.inner(np.transpose(quaternion_estimate),np.transpose(quaternion_estimate))**0.5)*quaternion_estimate  #normalisation
-print(quaternion_estimate) #convention is vector first then scalar
-#print(np.trace(B))
-#print(S)
-#print(rho*I - S)
-#print(np.linalg.inv(rho*I - S))
-#print(np.linalg.det(rho*I - S))
+    kai = np.trace(np.linalg.det(S)*np.linalg.inv(S))
+
+    z = z_func()
+
+    lambda_max = newtonRaphson(lambda_0, kai)
+    rho = lambda_max + np.trace(B)
+
+    temp = np.matmul(np.linalg.det(rho*I - S)*np.linalg.inv(rho*I - S), np.transpose(z))
+
+    quaternion_estimate = np.array([[temp[0]],[temp[1]],[temp[2]],[np.linalg.det(rho*I - S)]])
+    quaternion_estimate = (1/np.inner(np.transpose(quaternion_estimate),np.transpose(quaternion_estimate))**0.5)*quaternion_estimate  #normalisation
+    print(quaternion_estimate) #convention is vector first then scalar
+
+main()
+
+
 
