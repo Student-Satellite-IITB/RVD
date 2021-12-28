90b4d861cc361795572c3fb38a29630020a6adb3
Pari Vermani <55905816+parivermani@users.noreply.github.com>
2021-12-28 23:56:35 +0530

    Update calculated.py

--- Satellite Learning Tasks/Actuator Modelling/Problem Statement - Pari/calculated.py
+++ Satellite Learning Tasks/Actuator Modelling/Problem Statement - Pari/calculated.py
@@ -9,7 +9,7 @@ w_difference = np.zeros(1)
 w_difference[0] = 0
 temp = 0
 for i in range(1, int(CONTROL_TIME / time_step)):
-    temp = temp + (CURRENT[i] - CURRENT[i - 1]) * time_step * n * v_area[0] * v_B_BIB_m[0,0]
+    temp = temp + (CURRENT[i]) * time_step * n * v_area[0] * v_B_BIB_m[0,0]
     w_calculated = np.append(w_calculated, temp + w_calculated[i-1])
     w_difference = np.append(w_difference, math.fabs(w_calculated[i]-w_analytical[i])*100/w_analytical[i])
 
