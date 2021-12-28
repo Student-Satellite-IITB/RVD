ea74db15f908efbbf22bf782653aa0a0725184b4
Pari Vermani <55905816+parivermani@users.noreply.github.com>
2021-12-28 23:57:08 +0530

    Update analytical.py

--- Satellite Learning Tasks/Actuator Modelling/Problem Statement - Pari/analytical.py
+++ Satellite Learning Tasks/Actuator Modelling/Problem Statement - Pari/analytical.py
@@ -6,7 +6,7 @@ w_analytical[0] = w_initial
 mag_field = v_B_BIB_m[0, 0]
 area = v_area[0]
 for i in range(1, int(CONTROL_TIME / time_step)):
-    int_i = PWM_AMPLITUDE * d * time_step / RESISTANCE - PWM_AMPLITUDE * INDUCTANCE * (
+    int_i = int_i + PWM_AMPLITUDE * d * time_step / RESISTANCE - PWM_AMPLITUDE * INDUCTANCE * (
             math.exp(-RESISTANCE * time_step * (1 - d) / INDUCTANCE) - math.exp(
         -RESISTANCE * time_step / INDUCTANCE)) / math.pow(
         RESISTANCE, 2) + CURRENT[i] * INDUCTANCE * (1 - math.exp(-RESISTANCE * time_step / INDUCTANCE)) / RESISTANCE
